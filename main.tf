355321fe9ac52f21be5f7fc62f117208b631261d
ary.suwandi <ary.suwandi@izeno.com>
2025-04-17 15:36:17 +0700

    change back to hello world

--- main.tf
+++ main.tf
@@ -1,20 +1,3 @@
 output "hello_world" {
   value = "Hello, Ary"
 }
-
-terraform {
-  required_providers {
-    restapi = {
-      source  = "Mastercard/restapi"
-      version = "1.18.0"
-    }
-  }
-}
-
-provider "restapi" {
-  uri = "https://izeno-devops-0325.atlassian.net"
-  headers = {
-    Authorization = "Basic ${var.jira_token}"  # base64 encoded "email:api_token"
-    Content-Type  = "application/json"
-  }
-}
