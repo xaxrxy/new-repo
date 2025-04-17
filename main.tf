3dbfa7d2a62921d23441379631c4d01b4814d4cf
ary.suwandi <ary.suwandi@izeno.com>
2025-04-17 15:33:59 +0700

    change to terraform cloud yaml

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
 output "hello_world" {
   value = "Hello, Ary"
 }
+
+terraform {
+  required_providers {
+    restapi = {
+      source  = "Mastercard/restapi"
+      version = "1.18.0"
+    }
+  }
+}
+
+provider "restapi" {
+  uri = "https://izeno-devops-0325.atlassian.net"
+  headers = {
+    Authorization = "Basic ${var.jira_token}"  # base64 encoded "email:api_token"
+    Content-Type  = "application/json"
+  }
+}
