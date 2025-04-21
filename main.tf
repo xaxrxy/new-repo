816d9e1fe77dfcc4bcbcea80a36d0bb659e7c2e3
ary.suwandi <ary.suwandi@izeno.com>
2025-04-21 14:47:39 +0700

    add datetime

--- main.tf
+++ main.tf
@@ -14,7 +14,12 @@ output "hello_message" {
 provider "http" {}
 
 resource "null_resource" "jira_post_request_2" {
+  triggers = {
+    timestamp = timestamp()
+  }
+
    provisioner "local-exec" {
+     command = "echo Posting to Jira at ${timestamp()}"
      command = <<EOT
        curl -X POST https://izeno-devops-0325.atlassian.net/rest/api/2/issue/MSD-2/transitions \
          -H "Authorization: Basic YXJ5LnN1d2FuZGlAaXplbm8uY29tOkFUQVRUM3hGZkdGMDY2YUpTVW4tcVk1OFJJLWhZSUVuejk2a2I1X1pueXhpbmc2QXFDOG5qOUFaV1RQY0ZZUTJYOXBPdE93U0sxWktRVXlaTGFZdHFGTDVFSFNqZVZTdndmcWtTRjZxak5YS3cxVXhyN25Say03Qzl5MWJUVW5WYjF0VHdPUzlBb2M2akJ3ZzBZb18tS3Vfa3ZSZFJBY21uTnBRRUljOEI2YTRhaWxxaUJuT19BVT0yRThCQjY3OA==" \
