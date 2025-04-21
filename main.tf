99c2edfbe18ecf350f640aae4c20f2214c16eab2
ary.suwandi <ary.suwandi@izeno.com>
2025-04-21 13:33:58 +0700

    fixing 2 file.tf

--- main.tf
+++ main.tf
@@ -23,17 +23,13 @@ resource "local_file" "example" {
   content  = "This file was created by Terraform!!"
 }
 
-
 resource "null_resource" "jira_post_request" {
    provisioner "local-exec" {
      command = <<EOT
-       set -e
-       echo "Sending POST request to JIRA..."
-       RESPONSE=$(curl -s -w "\\nHTTP_STATUS_CODE:%{http_code}" -X POST https://izeno-devops-0325.atlassian.net/rest/api/2/issue/MSD-2/transitions \
+       curl -X POST https://izeno-devops-0325.atlassian.net/rest/api/2/issue/MSD-2/transitions \
          -H "Authorization: Basic YXJ5LnN1d2FuZGlAaXplbm8uY29tOkFUQVRUM3hGZkdGMDY2YUpTVW4tcVk1OFJJLWhZSUVuejk2a2I1X1pueXhpbmc2QXFDOG5qOUFaV1RQY0ZZUTJYOXBPdE93U0sxWktRVXlaTGFZdHFGTDVFSFNqZVZTdndmcWtTRjZxak5YS3cxVXhyN25Say03Qzl5MWJUVW5WYjF0VHdPUzlBb2M2akJ3ZzBZb18tS3Vfa3ZSZFJBY21uTnBRRUljOEI2YTRhaWxxaUJuT19BVT0yRThCQjY3OA==" \
          -H "Content-Type: application/json" \
-         -d '{"transition": {"id": 21}}')
-       echo "$RESPONSE"
+         -d '{"transition": {"id": 21}}'
      EOT
    }
  }
