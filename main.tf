66e96739ac468985266d080c17281c852900a668
Ferdinand . <bertaruh.rindoe@gmail.com>
2025-04-21 17:05:30 +0700

    Kucing Garong 1

--- main.tf
+++ main.tf
@@ -9,6 +9,21 @@ terraform {
 
 provider "http" {}
 
+resource "null_resource" "jira_get_transitions" {
+  triggers = {
+    always_run = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "Fetching transitions..."
+      curl -X GET https://izeno-devops-0325.atlassian.net/rest/api/3/issue/MSD-2/transitions \
+        -H "Authorization: Basic YXJ5LnN1d2FuZGlAaXplbm8uY29tOkFUQVRUM3hGZkdGMDY2YUpTVW4tcVk1OFJJLWhZSUVuejk2a2I1X1pueXhpbmc2QXFDOG5qOUFaV1RQY0ZZUTJYOXBPdE93U0sxWktRVXlaTGFZdHFGTDVFSFNqZVZTdndmcWtTRjZxak5YS3cxVXhyN25Say03Qzl5MWJUVW5WYjF0VHdPUzlBb2M2akJ3ZzBZb18tS3Vfa3ZSZFJBY21uTnBRRUljOEI2YTRhaWxxaUJuT19BVT0yRThCQjY3OA==" \
+        -H "Content-Type: application/json"
+    EOT
+  }
+}
+
 resource "null_resource" "jira_post_request_3" {
   triggers = {
     always_run = timestamp()
